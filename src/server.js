bf1813820f9492a47ceb3812577372dcfc1dc390
Tautvydas Tunkis <ttunkis@gmail.com>
2022-05-27 10:16:49 +0300

    cors on

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/', authRoutes);
